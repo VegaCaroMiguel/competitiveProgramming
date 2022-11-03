5e46d891de240d1db8ede36dea44222a080c0e08
Juan Miguel Vega Caro <jm.vegac@uniandes.edu.co>
2022-11-03 07:17:31 -0500

    Added another problem :D

---
+++ CodeForces/GravityFlip.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+
+    vector<int> vect;
+    while(a--){
+        int x;
+        cin>>x;
+        vect.push_back(x);
+    }
+    sort(vect.begin(), vect.end());
+    for(int i =0; i < vect.size(); ++i){
+        cout<<vect[i]<<" ";
+    }
+    return 0;
+}

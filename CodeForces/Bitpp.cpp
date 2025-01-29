b40e6eeabb9cc162e632ae124c14f72cf9d9021b
Miguel Vega <jm.vegac@uniandes.edu.co>
2025-01-28 20:02:54 -0500

    Update Bitpp.cpp

--- CodeForces/Bitpp.cpp
+++ CodeForces/Bitpp.cpp
@@ -1,28 +1,12 @@
-#include <iostream>
-#include <string>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
+    int a,b;
+    a = 10;
+    b = 20;
 
-    string aux;
-    int x = 0;
-    for(int i =0; i < n ; ++i){
-        cin>>aux;
-        if(aux == "X++"){
-            ++x;
-        }
-        else if(aux == "++X"){
-            ++x;
-        }
-        else if(aux == "X--"){
-            --x;
-        }
-        else if(aux == "--X"){
-            --x;
-        }
-    }
-    cout<<x<<endl;
-    return 0;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    int c = a+b;
+    cout<<c<<endl;
 }
